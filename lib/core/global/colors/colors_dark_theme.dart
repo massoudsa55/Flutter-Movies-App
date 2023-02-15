c355a3f3ded2717529cfae09f3c2488895ff719c
Messaoud_Ben <33179371+massoudsa55@users.noreply.github.com>
2023-02-15 22:37:24 +0100

    add global colors and  theme

--- lib/core/global/colors/colors_dark_theme.dart
+++ lib/core/global/colors/colors_dark_theme.dart
@@ -2,4 +2,10 @@ import 'package:flutter/material.dart';
 
 class AppColorLightTheme {
   static const Color background = Colors.green;
+  //RGB: 13, 37, 63
+  static const kPrimaryColor = Color(0xFF0d253f);
+  // RGB: 1, 180, 228
+  static const kSecondaryColor = Color(0xFF01b4e4);
+  // RGB: 144, 206, 161
+  static const kTertiaryColor = Color(0xFF90cea1);
 }
